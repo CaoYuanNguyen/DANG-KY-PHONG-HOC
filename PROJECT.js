538c6554a89633b9a61d98c12e8a3676b388462d
CaoYuanNguyen <161573034+CaoYuanNguyen@users.noreply.github.com>
2024-07-19 16:41:39 +0700

    Add files via upload
    first push

---
+++ PROJECT.js
@@ -0,0 +1,15 @@
+import { thongTinDangKy } from "./DKPHPAGE/DKPHPAGE.js";
+
+const dangKyButton = document.getElementById('dangKyButton');
+
+dangKyButton.addEventListener('click',()=>{
+    console.log(thongTinDangKy);
+    // window.location.href = './DKPHPAGE/DKPHPAGE.html';
+
+})
+
+
+
+
+
+
